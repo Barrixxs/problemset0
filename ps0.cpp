24e1db6b489a03b771bebec5fba2f5f85f72dfae
Barrixxs <73799364+Barrixxs@users.noreply.github.com>
2020-11-01 13:36:18 -0800

    Add files via upload

---
+++ ps0.cpp
@@ -0,0 +1,16 @@
+#include <iostream> // cout
+
+int main()
+{
+  std::cout << "CS135 Problem Set 0\n"
+            << "Kevin\n\n";
+  std::cout << "KK    KKK     BBBBBBBB   \n"
+            << "KK   KKK      BB     BB  \n"
+            << "KK  KK        BB     BB  \n"
+            << "KKKKK         BB     BB  \n"
+            << "KK  KKK       BBBBBBB    \n"
+            << "KK    KKK     BB     BB  \n"
+            << "KK      KK    BB     BB  \n"
+            << "KK      KK    BBBBBBBB   \n";
+  return 0;
+}
